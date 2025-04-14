2dcb433d26fc7ed79ddcb5efb108cb4e8e8c2610
Karalynn <karalynn.krause@gmail.com>
2025-04-14 16:15:39 -0400

    Basics

--- karalynnj.io/src/App.jsx
+++ karalynnj.io/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Box, Link, Stack, Typography } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Stack
+      spacing={2}
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <Box>
+        <Typography variant="h5">Hi, I'm Karalynn Jones</Typography>
+        <Typography variant="h6">I'm a Developer & Designer</Typography>
+      </Box>
+      <Stack direction={"column"}>
+        <Typography>
+          <FontAwesomeIcon icon={faGithub} />
+          <Link href="https://github.com/KaralynnJ" target="_blank">
+            KaralynnJ
+          </Link>
+        </Typography>
+        <Typography>
+          <FontAwesomeIcon icon={faLinkedin} />
+          <Link
+            href="https://ca.linkedin.com/in/karalynnmjones"
+            target="_blank"
+          >
+            Karalynn Jones
+          </Link>
+        </Typography>
+      </Stack>
+    </Stack>
+  );
 }
 
-export default App
+export default App;
